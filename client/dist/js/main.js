0356ddfcea726f60f76243f9f67052ea7610c4bc
Kenneth Quintero <kennethgqv@gmail.com>
2021-07-12 11:06:23 -0600

    Add behavior to sidebar and modify .gitignore file

--- client/dist/js/main.js
+++ client/dist/js/main.js
@@ -2,26 +2,6 @@ const transcriptContainer = document.getElementById("transcript-container");
 const micOn = document.getElementById("mic-on");
 const micOff = document.getElementById("mic-off");
 
-// temp begin
-const sidebar = document.getElementById("sidebar");
-const sidebarToggler = document.getElementById("sidebar-toggler");
-const blurDiv = document.getElementById("blur");
-let sidebarActive = false;
-
-sidebarToggler.onclick = () => {
-  if (sidebarActive) {
-    sidebarToggler.style.transform = "";
-    blurDiv.style.backdropFilter = "";
-    sidebar.style.left = "-180px";
-    sidebarActive = false;
-  } else {
-    sidebarToggler.style.transform = "rotateZ(180deg)";
-    blurDiv.style.backdropFilter = "blur(1px)";
-    sidebar.style.left = "0";
-    sidebarActive = true;
-  }
-};
-
 const recognition = new webkitSpeechRecognition();
 let recognitionListening = false;
 recognition.continuous = true;
@@ -97,3 +77,23 @@ async function translate(text, language) {
 
   return translation;
 }
+
+// sidebar
+const sidebar = document.getElementById("sidebar");
+const sidebarToggler = document.getElementById("sidebar-toggler");
+const blurDiv = document.getElementById("blur");
+let sidebarActive = false;
+
+sidebarToggler.onclick = () => {
+  if (sidebarActive) {
+    sidebarToggler.style.transform = "";
+    blurDiv.style.backdropFilter = "";
+    sidebar.style.left = "-180px";
+    sidebarActive = false;
+  } else {
+    sidebarToggler.style.transform = "rotateZ(180deg)";
+    blurDiv.style.backdropFilter = "blur(1px)";
+    sidebar.style.left = "0";
+    sidebarActive = true;
+  }
+};
