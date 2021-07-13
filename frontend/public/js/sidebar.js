2526976376270ef2f0a6a5832d6317375461715f
Kenneth Quintero <kennethgqv@gmail.com>
2021-07-12 23:09:14 -0600

    Add fetus version of dark theme

--- frontend/public/js/sidebar.js
+++ frontend/public/js/sidebar.js
@@ -1,18 +1,44 @@
-const sidebar = document.getElementById("sidebar");
-const sidebarToggler = document.getElementById("sidebar-toggler");
-const blurDiv = document.getElementById("blur");
-let sidebarActive = false;
+const root = document.documentElement;
+const darkModeSwitch = document.getElementById("dark-mode-switch");
+const sidebarIcon = document.getElementById("sidebar-icon");
+const micOffIcon = document.getElementById("mic-off-icon");
+const micOnIcon = document.getElementById("mic-on-icon");
 
-sidebarToggler.onclick = () => {
+const blurDiv = document.getElementById("blur");
+const sidebar = document.getElementById("sidebar");
+const sidebarTogglerContainer = document.getElementById("sidebar-toggler");
+
+// show and hide sidebar
+let sidebarActive = false;
+sidebarTogglerContainer.onclick = () => {
   if (sidebarActive) {
-    sidebarToggler.style.transform = "";
+    sidebarTogglerContainer.style.transform = "";
     blurDiv.style.backdropFilter = "";
-    sidebar.style.left = "-180px";
+    sidebar.style.left = "-250px";
+    sidebarTogglerContainer.style.left = "";
     sidebarActive = false;
   } else {
-    sidebarToggler.style.transform = "rotateZ(180deg)";
+    sidebarTogglerContainer.style.transform = "rotateZ(180deg)";
     blurDiv.style.backdropFilter = "blur(1px)";
     sidebar.style.left = "0";
+    sidebarTogglerContainer.style.left = "280px";
     sidebarActive = true;
+  }
+};
+
+// darkmode
+darkModeSwitch.onclick = () => {
+  if (darkModeSwitch.checked) {
+    root.style.setProperty("--background-color", "#212529");
+    root.style.setProperty("--foreground-color", "#fff");
+    sidebarIcon.style.fill = "white";
+    micOffIcon.style.fill = "white";
+    micOnIcon.style.fill = "white";
+  } else {
+    root.style.setProperty("--background-color", "#fff");
+    root.style.setProperty("--foreground-color", "#212529");
+    sidebarIcon.style.fill = "black";
+    micOffIcon.style.fill = "black";
+    micOnIcon.style.fill = "black";
   }
 };
