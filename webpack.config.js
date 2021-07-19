0a0e4dccb16d20a7bfeb5a49ba31014f1b86f20d
Kenneth Quintero <kennethgqv@gmail.com>
2021-07-19 14:27:28 -0600

    Add MiniCssExtractPlugin, using it only for building
    - If you run npm start, webpack will use devServer and inject the css in JS.
    - If you run npm build, webpack will extrac the css and output it to a separate file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const path = require("path");
 
-module.exports = {
+// this will receive the value of the task we ran
+const currentTask = process.env.npm_lifecycle_event;
+
+// plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// configuration optimazed for development
+config = {
+  plugins: [], // we'll fill this depending in our needs
+  output: {
+    filename: "main.js",
+    clean: true,
+  },
   devServer: {
     port: 6969,
     contentBase: path.resolve(__dirname, "dist"),
@@ -19,9 +31,25 @@ module.exports = {
         },
       },
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
 };
+
+// prettier-ignore
+if (currentTask == "build") {
+  // remove the style-loader to prevent injection of CSS in JS
+  config.plugins.push(new MiniCssExtractPlugin({ filename: "main.css" }));
+
+  // add the mini-css's loader
+  config.module.rules[1].use[0] = MiniCssExtractPlugin.loader;
+  config.module.rules[2].use[0] = MiniCssExtractPlugin.loader;
+}
+
+module.exports = config;
