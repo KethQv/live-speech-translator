e29799924ceea490f2bf316edbab94acf5b46997
Kenneth Quintero <kennethgqv@gmail.com>
2021-07-28 17:50:32 -0600

    Change app code to use google translate api not npm package ugh

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
-const translate = require("@vitalets/google-translate-api");
+// here get the api key of heroku environment
+
+const googleTranslate = require("google-translate")(API_KEY);
 const express = require("express");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 6969;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    languages: translate.languages,
-  });
+  res.render("index.ejs");
 });
 
 app.post("/translate", (req, res) => {
-  getTranslation(req.body.text, { from: req.body.from, to: req.body.to })
-    .then((translation) => {
-      res.send(translation);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  googleTranslate.translate(req.body.text, req.body.to, (err, translation) => {
+    if (err) console.log(err);
+    res.send(translation.translatedText);
+  });
 });
 
-app.listen(process.env.PORT || 8000);
-
-async function getTranslation(text, language) {
-  const response = await translate(text, language);
-  const translation = response.text;
-  return translation;
-}
+app.listen(port, () => {
+  console.log(`Express server listening at http://localhost:${port}`);
+});
