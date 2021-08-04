34ebe2f7986824c2e88bb3131f5ac051fe8d111b
Kenneth Quintero <kennethgqv@gmail.com>
2021-08-04 05:22:49 -0600

    Add endpoint /get-languages that gives all languages available as a response - require dotenv package to pull the api key

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// here get the api key of heroku environment
-
-const googleTranslate = require("google-translate")(API_KEY);
+require("dotenv").config();
+const googleTranslate = require("google-translate")(process.env.GOOGLE_API_KEY);
 const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 6969;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src"));
@@ -14,8 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/get-languages", (req, res) => {
+  googleTranslate.getSupportedLanguages("en", (err, languageCodes) => {
+    err ? console.log(err) : res.send(languageCodes);
+  });
 });
 
 app.post("/translate", (req, res) => {
